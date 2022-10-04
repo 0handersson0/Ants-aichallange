417cd16ac34376e6e55f8cbae6143979cac73795
Ola Winther <ola.winther@tietoevry.com>
2022-10-04 19:12:50 +0200

    Klonar starter package från https://github.com/wintheriscomming/ants-aichallenge

--- ants_aichallenge.ps1
+++ ants_aichallenge.ps1
@@ -9,19 +9,31 @@
    ^^  // \\
 
 "@
+
+if ((Test-Path $Path)) {
+    Write-Information -MessageData "Ants aichallenge is already setup. To run setup again, please remove the folder '$Path' first." -InformationAction Continue
+    return
+}
+
 Write-Information -MessageData "Running ants aichallenge setup" -InformationAction Continue
 Write-Information -MessageData "Create new folder $Path" -InformationAction Continue
 New-Item -Path $Path -ItemType Directory
-Write-Information -MessageData "Download c# starter pack" -InformationAction Continue
-Invoke-WebRequest -Uri https://lemon-water-079a32603.1.azurestaticapps.net/csharp_starter_package_2022.zip -OutFile $Path\csharp_starter_package_2022.zip
+
+Write-Information -MessageData "Cloning c# starter pack" -InformationAction Continue
+& git clone "https://github.com/wintheriscomming/ants-aichallenge" "$Path\ants\"
+Push-Location "$Path\ants\"
+& git remote rm origin
+Pop-Location
+
 Write-Information -MessageData "Download tools" -InformationAction Continue
 Invoke-WebRequest -Uri http://ants.aichallenge.org/tools.zip -OutFile $Path\tools.zip
+
 Write-Information -MessageData "Unpacking files" -InformationAction Continue
-Expand-Archive $Path\csharp_starter_package_2022.zip -DestinationPath $Path\csharp_starter_package_2022
-Expand-Archive $Path\tools.zip -DestinationPath $Path
+Expand-Archive $Path\tools.zip -DestinationPath $Path -Force
+
 Write-Information -MessageData "Removing zip files" -InformationAction Continue
 Remove-Item $Path\tools.zip
-Remove-Item $Path\csharp_starter_package_2022.zip
+
 $p = & { python -V } 2>&1
 if ($p -is [System.Management.Automation.ErrorRecord]) {
     Write-Information -MessageData $p.Exception.Message -InformationAction Continue
@@ -36,6 +48,7 @@ else {
     Write-Information -MessageData $p -InformationAction Continue
     Write-Information -MessageData "Python installed" -InformationAction Continue
 }
+
 Write-Information -MessageData "Downloading.Net SDK 6.0.401-x64" -InformationAction Continue
 Invoke-WebRequest -Uri https://download.visualstudio.microsoft.com/download/pr/cebf08ce-ecf1-4439-8a0a-d81b3a4cad12/674ba293b83bdc9b1e00ddfa3ab82f10/dotnet-sdk-6.0.401-win-x64.exe -OutFile $Path\dotnet-sdk-6.0.401-win-x64.exe
 Write-Information -MessageData "Installing .Net SDK 6.0.401-x64" -InformationAction Continue
@@ -43,19 +56,11 @@ Start-Process -FilePath “$Path\dotnet-sdk-6.0.401-win-x64.exe” -NoNewWindow 
 Write-Information -MessageData "Remove .Net SDK 6.0.401-x64 installation file" -InformationAction Continue
 Remove-Item $Path\dotnet-sdk-6.0.401-win-x64.exe
 Set-Location -Path $Path
-Write-Information -MessageData "Creating .Net projekt" -InformationAction Continue
-& dotnet new console --name ants -f net6.0
-Write-Information -MessageData "Move starter pack to project location" -InformationAction Continue
-Copy-Item -Path "$Path\csharp_starter_package_2022\*" -Destination "$Path\ants\" -Recurse
-Write-Information -MessageData "Remove old starter pack folder" -InformationAction Continue
-Remove-Item $Path\csharp_starter_package_2022\ -Recurse
-Write-Information -MessageData "Remove templete Program.cs" -InformationAction Continue
-Remove-Item $Path\ants\Program.cs -Recurse
+
 Write-Information -MessageData "Open application folder" -InformationAction Continue
-Set-Location -Path $Path\ants
-New-Item "Run.cmd" 
-Set-Content "Run.cmd" 'python ../tools/playgame.py "bin\Debug\net6.0\ants.exe" "python ../tools/sample_bots/python/HunterBot.py" --map_file ../tools/maps/example/tutorial1.map --log_dir game_logs --turns 60 --scenario --food none --player_seed 7 -e'
 & Start-Process .
+& Start-Process "$Path\ants\ants.sln"
+& dotnet build "$Path\ants\"
 Start-Process "http://ants.aichallenge.org/ants_tutorial.php"
 Write-Information -MessageData "Setup complete" -InformationAction Continue
 Write-Information -MessageData "Happy coding!!" -InformationAction Continue
